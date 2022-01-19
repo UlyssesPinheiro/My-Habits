9416fd355c9060f049786b9c0d47aaad5e1d2403
UlyssesPinheiro <ulyssespinheiro00@gmail.com>
2022-01-19 17:08:46 -0300

    added function createCircles

--- JavaScript/controller.js
+++ JavaScript/controller.js
@@ -1,3 +1,7 @@
 import * as module from "./model";
+import * as habitProgressView from "./Views/habitProgressView";
+import View from "./Views/view";
 
 // module.testing();
+
+habitProgressView.createCircles();
